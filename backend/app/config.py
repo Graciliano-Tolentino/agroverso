5aa9a9309984e5fedfeadad551150a2354d243cd
Graciliano-Tolentino <graciliano.tolentino@fatec.sp.gov.br>
2025-04-06 23:20:53 -0300

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+# Carrega variáveis do .env
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'agroverso-secreto')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///agroverso.db')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JSON_SORT_KEYS = False
-    CORS_HEADERS = 'Content-Type'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
