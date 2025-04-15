283ab0d929743c7db52de05d20ebfb779295a05d
Graciliano Tolentino <graciliano.tolentino@fatec.sp.gov.br>
2025-04-15 16:08:12 -0300

    Corrige App.jsx para exibir interface do Agroverso

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+// 🌿 App.jsx – Estrutura principal da aplicação Agroverso com roteamento
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Home from '@pages/Home'
+import Sobre from '@pages/Sobre'
+import Contato from '@pages/Contato'
+import Admin from '@pages/Admin'
+import Logout from '@pages/Logout'
+import NotFound from '@pages/404'
 
 export default function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
